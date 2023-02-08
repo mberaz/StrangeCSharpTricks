28fed618dc58bc98881448b74134ae14deba7946
michael berezin <mbearz@gmail.com>
2023-02-08 19:49:06 +0200

    list

--- StrangeCSharpTricks/Controllers/ExportController.cs
+++ StrangeCSharpTricks/Controllers/ExportController.cs
@@ -61,5 +61,31 @@ namespace StrangeCSharpTricks.DictionaryIsTheNewIf.Controllers
             var bytes = ExportToExcel.ExportWorksheets(WorksheetDataModelCreator.CreateWorksheetDataModel(list, "ClassWithHeaderList", headerNames));
             return ContentHelper.ToXlsxFile(bytes, "ClassWithHeaderList");
         }
+
+        [HttpGet("List")]
+        public async Task<IActionResult> List()
+        {
+            var list = new List<ClassWithAttributeName>
+            {
+                new ClassWithAttributeName{Email = "a.b@mail.com",FirstName = "a", LastName = "b",Id = 1},
+                new ClassWithAttributeName{Email = "c.d@mail.com",FirstName = "c", LastName = "d",Id = 2},
+                new ClassWithAttributeName{Email = "e.f@mail.com",FirstName = "e", LastName = "f",Id = 2},
+            };
+            var list2 = new List<ClassWithHeader>
+            {
+                new ClassWithHeader{Email = "a.b@mail.com",FirstName = "a", LastName = "b",Id = 1},
+                new ClassWithHeader{Email = "c.d@mail.com",FirstName = "c", LastName = "d",Id = 2},
+                new ClassWithHeader{Email = "e.f@mail.com",FirstName = "e", LastName = "f",Id = 2},
+            };
+
+            var headerNames = new List<string> { "id", "name", "second name", "email" };
+
+            var bytes = ExportToExcel.ExportWorksheets(new List<WorksheetDataModel>
+            {
+                WorksheetDataModelCreator.CreateWorksheetDataModel(list, "ClassWithAttributeName"),
+                WorksheetDataModelCreator.CreateWorksheetDataModel(list2, "ClassWithHeaderList", headerNames)
+            });
+            return ContentHelper.ToXlsxFile(bytes, "ClassWithAttributeName");
+        }
     }
 }
